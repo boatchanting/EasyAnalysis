53b51ba8a9752fafe7196874fecf2114f8c35bd9
Boatchanting <1440068386@qq.com>
2025-01-11 17:40:36 +0800

    更新文件树

--- 生成文件树.py
+++ 生成文件树.py
@@ -12,8 +12,8 @@ def generate_file_tree_recursive(dir_path, current_depth, max_depth, file, prefi
     for i, item in enumerate(files):
         item_path = os.path.join(dir_path, item)
         
-        # Skip __pycache__ folder
-        if item == '__pycache__':
+        # Skip __pycache__ folder, .git folder, .vscode folder, and 生成文件树.py file
+        if item in ['__pycache__', '.git', '.vscode', '生成文件树.py',".gitignore","data_loader原.py","data_analysis原.py","example","test","test折线图.py"]:
             continue
         
         if os.path.isdir(item_path):
@@ -35,6 +35,6 @@ def generate_file_tree_recursive(dir_path, current_depth, max_depth, file, prefi
                 file.write(f'{prefix}├── {item}\n')
 
 if __name__ == "__main__":
-    dir_path = r"D:\高行周的资料\大二上\analysis_app\v1"
+    dir_path = r"."
     generate_file_tree(dir_path)
     print(f"文件树已生成并保存为 file_tree.txt")
